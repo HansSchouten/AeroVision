f56ccf66c371e98aa21d98fe86ba7b32c84c18b9
Hans Schouten <account@emailo.nl>
2019-04-13 19:18:00 +0200

    Add support for command line arguments

--- aerovision.py
+++ aerovision.py
@@ -1,4 +1,4 @@
-import sys
+import sys, getopt
 
 from aerovision.parsers import FlightParser
 from aerovision.kml.generators.animation import *
@@ -9,12 +9,37 @@ def main(argv):
 	AeroVision entry point.
 
 	"""
+	# default arguments
+	flightsFile = "data/flights.csv"
+	outFile = "output/flights.kml"
+	visualisation = "animation"
+
+	# parse command line arguments
+	try:
+		opts, args = getopt.getopt(argv,"hf:o:v:",["flights=","out=","visualisation="])
+	except getopt.GetoptError:
+		print('aerovision.py -f <flightsFile> -o <outputfile> -v timespan|animation')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print('aerovision.py -f <flightsFile> -o <outputfile> -v timespan|animation')
+			sys.exit()
+		if opt in ('-f', '--flights'):
+			flightsFile = arg
+		elif opt in ('-o', '--out'):
+			outFile = arg
+		elif opt in ('-v', '--visualisation'):
+			visualisation = arg
+			
 	parser = FlightParser()
-	flights = parser.getFlights("data/flights.csv")
+	flights = parser.getFlights(flightsFile)
 	
-	generator = FlightsKMLTimespan(flights)
-	generator.generateKML("output/flights.kml")
+	generator = FlightsKMLAnimation(flights)
+	if visualisation == 'timespan':
+		generator = FlightsKMLTimespan(flights)
+
+	generator.generateKML(outFile)
 
 
 if __name__ == "__main__":
-	main(sys.argv)
+	main(sys.argv[1:])
